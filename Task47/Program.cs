a317ffced24f509cc176df081ee38ebb727f5c34
Natahis <natahis@bk.ru>
2022-08-28 22:03:46 +0300

    Task47

---
+++ Task47/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+double[,] GreateMatrixRndInt(int row, int col)
+{
+    double[,] matrix = new double[row, col];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Convert.ToDouble(new Random().Next(-100, 100))/10;
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(j < matrix.GetLength(1) - 1?$"{matrix[i, j], 4};":$"{matrix[i, j], 4}");
+        }
+        Console.WriteLine("]");
+    }
+}
+double[,] arr2D = GreateMatrixRndInt(3, 4);
+PrintMatrix(arr2D);
